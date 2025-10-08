3d3dd7631078ca9d15d238592c3caf344ce3cc0a
lathifah199 <lathifahkhalifatunisa@gmail.com>
2025-10-08 19:50:20 +0700

    latifah  frontend Halaman orang tua

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="bg-[#E9B9C5] py-4 text-left text-white">
+  <div class="max-w-[1200px] mx-auto flex flex-col items-start gap-3 px-4">
+    <h2 class="font-semibold text-xl tracking-wide">SKINKARE</h2>
+
+    <p class="text-base leading-relaxed">
+      Email: skinkare@gmail.com<br>
+      No. HP: 0812-3456-7890<br>
+      Media Sosial:
+    </p>
+
+    <!-- Link dan ikon media sosial -->
+    <div class="flex gap-5 mt-2">
+      <a href="https://www.instagram.com/polibatamofficial">
+        <img src="https://img.icons8.com/?size=100&id=32292&format=png&color=000000" 
+             alt="Instagram" class="w-[30px] h-[30px] filter invert">
+      </a>
+      <a href="https://www.tiktok.com/@polibatamofficial">
+        <img src="https://img.icons8.com/?size=100&id=84521&format=png&color=000000" 
+             alt="TikTok" class="w-[30px] h-[30px] filter invert">
+      </a>
+      <a href="https://www.linkedin.com/feed/">
+        <img src="https://img.icons8.com/?size=100&id=447&format=png&color=000000" 
+             alt="LinkedIn" class="w-[30px] h-[30px] filter invert">
+      </a>
+      <a href="https://chat.whatsapp.com/FEM3SNcya4qFfjOh6qEqEk">
+        <img src="https://img.icons8.com/?size=100&id=16712&format=png&color=000000" 
+             alt="WhatsApp" class="w-[30px] h-[30px] filter invert">
+      </a>    
+    </div>
+  </div>
+</footer>
+
+<!-- Copyright-->
+<div class="text-center text-xs text-gray-500 py-3 bg-[#F7F7F7]">
+  &copy; 2024 All rights reserved.
+</div>
