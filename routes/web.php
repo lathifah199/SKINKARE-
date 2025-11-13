31f6de1fdbf7af1d309a11011c0c4aee3ab07532
lathifah199 <lathifahkhalifatunisa@gmail.com>
2025-11-13 14:33:09 +0700

    sikit

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\BarcodeController;
 use App\Http\Controllers\RiwayatController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.halamanbf');
 });
 
 // ======================= AUTH =======================
